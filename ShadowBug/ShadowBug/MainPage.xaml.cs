a0e36d3f10ab539d147a734e800a11bd5d936176
Matt Soucoup <msoucoup@codemilltech.com>
2019-04-17 10:01:13 -0700

    initial add

---
+++ ShadowBug/ShadowBug/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShadowBug
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var childPage = new ChildPage();
+
+            await Navigation.PushAsync(childPage);
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
